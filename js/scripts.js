5714e3475297e33380d5875ae6f28be35efc08a7
Brian Henry and Kristi Hwang <brian@>
2018-04-02 15:09:52 -0700

    Add address object inside of persons object, update business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,86 @@
 // Business Logic
 
-function Person(first, last, phone, address, email){
-  this.firstName = first,
-  this.lastName = last,
-  this.phoneNumber = phone,
-  this.address = address,
-  this.email = email
+function Person(first, last, phone, addresses, email){
+  this.firstName = first;
+  this.lastName = last;
+  this.phoneNumber = phone;
+  this.addresses = [];
+  this.email = email;
 }
-function Address(number, street, city, state, zipcode){
-  this.number = number,
-  this.street = street,
-  this.city = city,
-  this.state = state,
-  this.zipcode = zipcode
+function Address(number, street, city, state, zip){
+  this.number = number;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
 }
 
-person.prototype.fullName = function() {
+Person.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
-address
+Address.prototype.fullAddress = function() {
+  return this.inputAddNumber + " " + this.inputAddStreet + ", " + this.inputCity + ", " + this.inputState + " " + this.inputZip;
+}
 
 // User Logic
 $(document).ready(function() {
-  $(form#newContact).submit(function(event) {
+  $("#newAddress").append('<div class="newAddress">' +
+                              '<div class="form-group">' +
+                                '<label for="number"> Number </label>' +
+                                '<input type="text" class="form-control number">' +
+                              '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="street"> Street </label>' +
+                                 '<input type="text" class="form-control street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="city"> City </label>' +
+                                 '<input type="text" class="form-control city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="state"> State </label>' +
+                                 '<input type="text" class="form-control state">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="zip"> Zip Code </label>' +
+                                 '<input type="text" class="form-control zip">' +
+                               '</div>' +
+                             '</div>');
+    });
+  $("form#newPerson").submit(function(event) {
     event.preventDefault();
 
     var inputFirst = $("input#first").val();
     var inputLast = $("input#last").val();
     var inputPhone = $("input#phone").val();
-    var inputAddress = $("input#address").val();
-    var inputState = $("").val();
-    var inputCity = $("").val();
-    var inputZip = $("").val();
     var inputEmail = $("input#email").val();
-
     var newPerson = new Person(inputFirst, inputLast, inputPhone, inputAddress, inputEmail);
 
-$("ul#directory").append("<li><span class= 'person'>" + newPerson.fullName() +
-"</span></li>");
+    var inputAddNumber = $(this).find("input.number").val();
+    var inputAddStreet = $(this).find("input.street").val();
+    var inputCity = $(this).find("input.city").val();
+    var inputState = $(this).find("input:radio[name=state]:checked").val();
+    var inputZip = $(this).find("input.zip").val();
+    var newAddress = new Address(inputAddNumber, inputAddStreet, inputCity, inputState, inputZip);
 
-$("directory").last().click(function(){
-  $("#show-person").show();
-  $("#show-person h2").text(newPerson.firstName);
-  $(".first").text(newPerson.firstName);
-  $(".last").text(newPerson.lastName);
-  });
-  $("input#newFirst").val("");
-  $("input#newLast").val("");
+    Person.addresses.push(newAddress);
+
+    $("ul#directory").append("<li><span class= 'person'>" + newPerson.fullName() +
+    "</span></li>");
+    $("directory").last().click(function(){
+      $("#show-person").show();
+      $("#show-person").text(newPerson.firstName);
+      $(".first").text(newPerson.firstName);
+      $(".last").text(newPerson.lastName);
+    });
+      $("input#newFirst").val("");
+      $("input#newLast").val("");
+
+
+    $("ul#addresses").text("");
+     newContact.addresses.forEach(function(address) {
+       $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+
+    });
   });
 });
